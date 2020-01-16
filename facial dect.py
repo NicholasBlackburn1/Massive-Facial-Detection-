83d6c9fb7915b98a15d8212032892db2be90d414
nicholas blackburn <nickblackburn02@gmail.com>
2020-01-16 14:11:49 -0500

    added face xml and base python file

---
+++ facial dect.py
@@ -0,0 +1,41 @@
+
+import cv2
+import cv2 as cv
+import sys
+
+font = cv2.FONT_HERSHEY_DUPLEX
+Path = "face.xml"
+faceCascade = cv2.CascadeClassifier(Path)
+
+video_capture = cv2.VideoCapture(0, cv2.CAP_V4L)
+
+while True:
+    # Capture frame-by-frame
+    ret, frame = video_capture.read()
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(40, 40),
+        flags=cv2.CASCADE_SCALE_IMAGE
+    )
+
+    # Draw a rectangle around the faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+
+        cv2.putText(frame, str(faces.shape[0]), (100, 100),
+                    font, 2.0, (0, 255, 0), 1)
+
+    # Display the resulting frame
+    cv2.imshow('Video', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything is done, release the capture
+video_capture.release()
+cv2.destroyAllWindows()
